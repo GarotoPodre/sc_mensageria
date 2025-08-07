a23b9022bc5ae380329745d5f809f1e1768b7f1d
Welington Andrade <andrade.wellington@gmail.com>
2025-08-07 09:40:13 -0300

    feat(service):infraestrutura de criacao de usuario adicionada

---
+++ src/main/java/br/com/gama/mensageria/service/MensageriaService.java
@@ -0,0 +1,22 @@
+package br.com.gama.mensageria.service;
+
+import br.com.gama.mensageria.dominio.Mensagem;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MensageriaService {
+    public static final String EXCHANGE_MENSAGENS ="mensagens.exchange";
+    public static final String ROUTING_KEY_MENSAGEM ="mensagens.nova";
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void sendMessage(String sender, String content) {
+
+        Mensagem mensagem = new Mensagem(sender, content);
+        rabbitTemplate.convertAndSend(EXCHANGE_MENSAGENS, ROUTING_KEY_MENSAGEM, mensagem);
+    }
+
+}
---
+++ src/test/java/br/com/gama/mensageria/service/MensageriaServiceTest.java
@@ -0,0 +1,59 @@
+package br.com.gama.mensageria.service;
+
+import br.com.gama.mensageria.dominio.Mensagem;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+
+import static br.com.gama.mensageria.service.MensageriaService.EXCHANGE_MENSAGENS;
+import static br.com.gama.mensageria.service.MensageriaService.ROUTING_KEY_MENSAGEM;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class MensageriaServiceTest {
+
+    /**
+     * @Mock cria um RabbitTemplate 'fake'.
+     * Isso elimina a necessidade de se conectar a um RabbitMQ Server
+     */
+    @Mock
+    private RabbitTemplate rabbitTemplate;
+
+    /**
+     * @InjectMocks cria um MensageriaService e injeta o rabbitTemplate fake nele
+     */
+    @InjectMocks
+    private MensageriaService mensageriaService;
+
+    /**
+     * Verifica se a mensagem foi enviada corretamente, da forma que foi criada
+     */
+    @Test
+    @DisplayName("Deve criar uma mensagem e envia-la para o RabbitMQ via RabbitTemplate")
+    void shouldSendMessageToRabbitMQ() {
+        //Arrange
+        String sender = "sender";
+        String content = "Oi, Mocks!";
+
+        //Act
+        mensageriaService.sendMessage(sender, content);
+
+        //Assert: verifica se o nosso serviço chamou o metodo correto no mock
+        ArgumentCaptor<Mensagem> mensagemCaptor = ArgumentCaptor.forClass(Mensagem.class);
+        verify(rabbitTemplate).convertAndSend(eq(EXCHANGE_MENSAGENS), eq(ROUTING_KEY_MENSAGEM), mensagemCaptor.capture());
+
+        //verifica se os valores foram corretamente enviados
+        Mensagem mensagemEnviada = mensagemCaptor.getValue();
+        assertEquals(sender, mensagemEnviada.getSender());
+        assertEquals(content, mensagemEnviada.getContent());
+
+    }
+}
