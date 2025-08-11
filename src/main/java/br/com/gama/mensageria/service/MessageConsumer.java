0e62f68a71832eb1c65ac288fae468c3c5d3159b
Welington Andrade <andrade.wellington@gmail.com>
2025-08-11 08:21:02 -0300

    refactor(consumer):Consumidor de mensagem adicionado e mensagem sendo enviada ao websockt, com RabbitListener

--- src/main/java/br/com/gama/mensageria/service/MessageConsumer.java
+++ src/main/java/br/com/gama/mensageria/service/MessageConsumer.java
@@ -4,13 +4,18 @@ import br.com.gama.mensageria.amqp.WebSocketHandler;
 import br.com.gama.mensageria.dominio.Mensagem;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Log4j2
 @Service
 @RequiredArgsConstructor
 public class MessageConsumer {
+    public static final String FILA_GENERICA_MENSAGENS = "mensagens.generica";
+
     private final WebSocketHandler webSocketHandler;
+
+    @RabbitListener(queues = FILA_GENERICA_MENSAGENS)
     public void consumirMensagem(Mensagem mensagem) {
 
         log.info("Mensagem recebida: {}", mensagem);
